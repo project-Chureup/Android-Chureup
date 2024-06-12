a60ac5951ea8fdbc77f39126c6e92579f362b92f
dahyun0917 <dahyun0917@naver.com>
2024-06-12 21:38:00 +0900

    design(#6): Color.kt , Theme.kt Setting
    [제목] Project Color,Theme Setting

--- app/src/main/java/money/android/chureup/ui/theme/Color.kt
+++ app/src/main/java/money/android/chureup/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package money.android.chureup.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val red = Color(0xFFFF1F1F)
+val yellow = Color(0xFFFFC803)
+val orange = Color(0xFFFF6D03)
+val light_gray = Color(0xFFF2F2F2)
+val dark_gray = Color(0x80000000)
+val description_gray = Color(0xFF8E8E8E)
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
